ae69f9a8ebbafcd1537e21a86e45d929802ce7ec
Myriam1501 <myriamo93500@gmail.com>
2022-11-09 16:23:26 +0100

    d

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     require_once('controllers/homepageController.php');
     require_once('controllers/figureController.php');
 
-
+    try {
         if(isset($_GET['action']) && '' !== $_GET['action']) {
             $action = $_GET['action'];
             
@@ -17,23 +17,16 @@
                 if ('list' === $action) {
                     $figureController->list();
                 }
-<<<<<<< HEAD
                 if ('update' === $action) {
                     $figureController->update();
-=======
-
-                if ('update' === $action) {
-                    if (!array_key_exists('id', $_GET)) {
-                        throw new \Exception('Id parameter is mandatory');
-                    }
-
-                    $figureController->update($_GET['id']);
->>>>>>> 8985026f68755f2a4dca0d67e7615d826fac5d9e
                 }
             }
         } else {
             (new HomepageController())->home();
         }
+    } catch (\Exception $exception) {
+        throw new \Exception($exception->getMessage());
+    }
 
 
 
